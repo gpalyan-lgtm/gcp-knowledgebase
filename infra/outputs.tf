4d7a2e93e48fb3bd79fb7ef474e3cf53c68e5d4c
gpalyan-lgtm <g.palyan@source-werbeartikel.com>
2025-10-29 16:04:36 +0100

    initial commit

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,13 @@
-# outputs.tf
-
 # The ID of the newly created knowledgebase project.
 output "project_id" {
-  description = "The ID of the newly created knowledgebase project."
+  description = "The ID of the knowledgebase project."
   value       = google_project.knowledgebase_project.project_id
+}
+
+# The GCP region where resources are deployed.
+output "gcp_region" {
+  description = "The GCP region where the resources are deployed."
+  value       = var.gcp_region
 }
 
 # The connection name of the Cloud SQL instance, used for connecting via the Cloud SQL Auth Proxy.
@@ -12,8 +16,26 @@ output "cloud_sql_instance_name" {
   value       = google_sql_database_instance.default.connection_name
 }
 
-# The public IP address of the Cloud SQL instance. Note: This is for initial setup and should be removed for production.
-output "cloud_sql_instance_public_ip" {
-  description = "The public IP of the Cloud SQL instance (for initial setup)."
-  value       = google_sql_database_instance.default.public_ip_address
+# The URL of the deployed Knowledgebase API service.
+output "api_service_url" {
+  description = "The main endpoint for the Knowledgebase API service."
+  value       = google_cloud_run_v2_service.api_service.uri
+}
+
+# The static IP address used for outbound traffic from the Cloud Run service.
+output "static_outbound_ip" {
+  description = "Static IP for egress traffic from the serverless VPC connector."
+  value       = google_compute_address.static_ip.address
+}
+
+# The name of the secret in Secret Manager containing the database password.
+output "db_password_secret_name" {
+  description = "The name of the Secret Manager secret for the DB password."
+  value       = google_secret_manager_secret.db_password_secret.secret_id
+}
+
+# The email of the service account used by the Knowledgebase API.
+output "api_service_account_email" {
+  description = "The service account email for the main API service."
+  value       = google_service_account.api_sa.email
 }
---
+++ infra/terraform.tfvars
@@ -0,0 +1,12 @@
+# The ID of the Billing Account
+billing_account_id = "billing_account"
+
+# A globally unique ID for the new Google Cloud project.
+# Must contain only lowercase letters, numbers, and hyphens.
+#project_id         = "your-unique-project-id"
+
+# The display name for the project.
+#project_name       = "Knowledgebase Production"
+
+# The region for the resources.
+# gcp_region         = "europe-west3"
